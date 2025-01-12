a78b6e0f7016190ed844909d51bedfd8c2c3cc13
Otro <gonzalezmartinnatanael@gmail.com>
2025-01-12 17:27:54 -0300

    comit

--- public/inicio/inicio.js
+++
@@ -1,6 +0,0 @@
-function redirect(page) {
-    const confirmRedirect = confirm("¿Estás seguro de que deseas continuar?");
-    if (confirmRedirect) {
-        window.location.href = page; // Redirige a la página correspondiente
-    }
-}
